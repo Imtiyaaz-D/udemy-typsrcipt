a3103c4ac08ff667e4a514765dfc8244f93e90b1
Imtiyaaz-D <129970424+Imtiyaaz-D@users.noreply.github.com>
2024-08-08 09:26:20 +0200

    varibales

---
+++ features/annotations/variables.ts
@@ -0,0 +1,16 @@
+let apples : number = 5;
+let speed : string  = 'woof'
+let hasName : boolean = true
+
+let nothingMuch : null = null;
+
+let nothing: undefined =  undefined
+
+//bulit in objects
+let now : Date  = new Date();
+
+
+//Array
+let colors : string [] = [ 'red' ,'green ', 'blue'];
+let myNumbers : number [] = [1,2,3];
+let truths : boolean  [] = [true]
